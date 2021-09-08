26c788cb1eaaf2a4d3e229a558b77283b967f69e
Mohamed Essam <68881655+Adidas69@users.noreply.github.com>
2021-09-08 14:09:39 +0200

    Add files via upload

---
+++ E or O.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
+	int number; 
+	cout <<"Enter your number:";
+	cin >>number;
+	if(number%2==0):
+		cout<<"The number is EVEN";
+	else
+		cout<<"The number is ODD";
